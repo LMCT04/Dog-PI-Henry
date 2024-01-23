1c7ab8393e37eb889174a3edbb721043135e18ae
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2024-01-22 23:43:07 -0300

    v4

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import { Home, Landing, Detail, Form } from "./views";
-import { Route, Routes, useLocation } from "react-router-dom";
-import NavBar from "./components/navBar/navBar";
+import { Home, Landing, Detail, Form } from './views'
+import{ Route, useLocation } from 'react-router-dom'
+import NavBar from './components/navBar/navBar'
+
 
 function App() {
-    const location = useLocation();
 
-    return (
-        <div className="App">
-            {location.pathname !== "/" && <NavBar />}
-            <Routes>
-                <Route exact path="/" component={Landing} />
-                <Route path="/home" component={Home} />
-                <Route exact path="/detail/:id" component={Detail} />
-                <Route exact path="/form" component={Form} />
-            </Routes>
-        </div>
-    );
+  const location = useLocation();
+
+  return (
+    <div className="App">
+
+      {  location.pathname !== '/' && <NavBar/>  }
+      <Route exact path='/' component = {Landing} />
+      <Route path='/home' component = {Home} />
+      <Route exact path='/detail/:id' component = {Detail} />
+      <Route exact path='/form' component = {Form} />
+    </div>
+  );
 }
 
 export default App;
