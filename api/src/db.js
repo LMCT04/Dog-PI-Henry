f099612d71736a0ad183536ace91795beae11384
LMCT04 <100221599+LMCT04@users.noreply.github.com>
2024-01-22 21:45:09 -0300

    ssl

--- api/src/db.js
+++ api/src/db.js
@@ -9,6 +9,13 @@ const {
 const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  ssl:true,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Ajusta esto en función de la configuración de tu base de datos
+    },
+  },
 });
 
 /*const sequelize = new Sequelize(DB_DEPLOY, {
